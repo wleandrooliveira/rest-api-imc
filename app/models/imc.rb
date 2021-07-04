4488ea0118e66704111579b63f058309ea1690b0
Wanderson <wleandro.oliveira@gmail.com>
2021-07-03 23:04:47 -0300

    create model imc

--- app/models/imc.rb
+++ app/models/imc.rb
@@ -1,2 +1,11 @@
 class Imc
+    attr_reader :imc
+    def initialize(height,weight)
+      @height = height
+      @weight = weight
+    end
+
+    def imc
+        @imc = (@weight/(@height * @height)).round(2)
+    end
 end
